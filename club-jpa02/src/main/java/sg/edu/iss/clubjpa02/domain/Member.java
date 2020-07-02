fe5ef5b76514ee6990cfd006fd094b8c38824564
lydia <lydia@LAPTOP-OURF3GHB.lan>
2020-07-02 23:10:00 +0800

    added constructors in Member.java

--- club-jpa02/src/main/java/sg/edu/iss/clubjpa02/domain/Member.java
+++ club-jpa02/src/main/java/sg/edu/iss/clubjpa02/domain/Member.java
@@ -8,5 +8,25 @@ public class Member {
 	private int memberId;
 	private String name;
 	private String password;
+	public Member() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public Member(String name, String password) {
+		super();
+		this.name = name;
+		this.password = password;
+	}
+	public Member(int memberId, String name, String password) {
+		super();
+		this.memberId = memberId;
+		this.name = name;
+		this.password = password;
+	}
+	@Override
+	public String toString() {
+		return "Member [memberId=" + memberId + ", name=" + name + ", password=" + password + "]";
+	}
 
+	
 }
